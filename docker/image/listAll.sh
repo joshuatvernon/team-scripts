8b0e321afb80bf5794bd46a249e5014fec611bfd
Joshua Vernon <jvernon@netflix.com>
2021-04-04 23:11:56 -0700

    Updated sh script

--- docker/image/listAll.sh
+++ docker/image/listAll.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "Listing all"
+VARIABLE=""
+
+while getopts 'v:h' OPT; do
+case "${OPT}" in
+    v)
+        VARIABLE="$OPTARG"
+        ;;
+    h|\?|:)
+        echo "help"
+        exit 0
+        ;;
+    *)
+        echo "help"
+        exit 0
+        ;;
+esac
+done
+
+echo "VERSION = $VERSION"
+echo "VARIABLE = $VARIABLE"
